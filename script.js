ce78280b2185423e44b3672fe4815c317d34d377
yamm10 <131327547+yamm10@users.noreply.github.com>
2024-10-01 10:37:28 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// JavaScript
+
+console.log('Hello world!');
+
+//画面が読み込まれたら animation を呼び出す
+window.addEventListener('load', animation);
+
+// 定数の定義
+const loading = document.getElementById('loading');
+
+// cssクラスを追加する関数
+function animation(){
+  loading.classList.add('loaded');
+}
